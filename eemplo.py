d1bf9b05e32968c9e06b038a55f7d6641c94e5e5
USRVI-LC12 <USRVI-LC12@duoc.acad>
2023-05-23 20:29:18 -0400

    ejercicios funciones y arreglos

---
+++ eemplo.py
@@ -0,0 +1,26 @@
+import funciones as fn
+
+#nombres = ["Juan","Maria","Jose"]
+nombres = []
+#for i in range(len(nombres)):
+#    print(nombres[i])
+
+for i in range(5):
+    print("Nombre", i)
+    nom = input("Ingrese el nombre: ")
+    nombres.append(nom)
+    
+for i in nombres:
+    print(i)
+
+for i in range(4):
+    nom  = input("Ingrese su nombre: ")
+    fn.saludo(nom)
+
+print(nom)
+
+añoNac = int(input("Ingrese su año de nacimiento"))
+fn.calcularEdad(añoNac)
+
+
+
---
+++ funciones.py
@@ -0,0 +1,8 @@
+def saludo(nombre):
+    print("hola",nombre)
+    print("Bienvenido a este mágico lenguaje de programación")
+
+def calcularEdad(añoNac):
+    añoActual = 2023
+    edad = añoActual - añoNac
+    print("Ud tiene ",edad, " años app")
